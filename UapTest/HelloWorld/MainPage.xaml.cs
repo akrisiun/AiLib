1d2ddfc3de9376c69516d6291dc0a8fd4f961705
AndriusK <akrisiun@gmail.com>
2015-10-13 12:14:49 +0300

    UAP HelloWorld project

---
+++ UapTest/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿// Copy left (c) Microsoft. no rights reserved.
+
+using System;
+using System.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            list.Items.Add(
+                    new ListBoxItem { Content = "Line 3" });
+
+            IDictionary env = System.Environment.GetEnvironmentVariables();
+
+            IDictionaryEnumerator num = env.GetEnumerator();
+            while (num.MoveNext())
+                list.Items.Add(
+                    new ListBoxItem { Content = num.Key + " = " + num.Value }
+                    );
+            // + Environment.NewLine;
+
+            this.Showdown.Click += (s, e)
+                =>
+            {
+                Application.Current.Exit();
+            };
+
+        }
+
+        private void ClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            HelloMessage.Text = "Hello, Windows 10 IoT Core!";
+        }
+    }
+}
---
+++ UapTest/HelloWorld/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+// MIT
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HelloWorldUap")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldUap")]
+[assembly: AssemblyCopyright("MIT 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
